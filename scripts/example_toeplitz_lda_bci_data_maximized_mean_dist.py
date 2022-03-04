4b4cf7cbe923d4a0c52d793c4e0338d1332454c0
Jan Sosulski <mail@jan-sosulski.de>
2022-03-04 17:30:42 +0100

    feat: unsupervised classification of llp datasets

--- scripts/example_toeplitz_lda_bci_data_maximized_mean_dist.py
+++ scripts/example_toeplitz_lda_bci_data_maximized_mean_dist.py
@@ -3,8 +3,6 @@ import time
 from pathlib import Path
 import matplotlib.pyplot as plt
 
-from sklearn.mixture._gaussian_mixture import _compute_precision_cholesky
-
 import numpy as np
 from blockmatrix import SpatioTemporalData
 
@@ -17,7 +15,6 @@ except ImportError:
     )
     exit(1)
 
-from sklearn.mixture import GaussianMixture
 import pandas as pd
 
 from toeplitzlda.classification import EpochsVectorizer
@@ -30,43 +27,15 @@ from toeplitzlda.usup_replay.visual_speller import (
 
 mne.set_log_level("ERROR")
 # %%
-""" Poor man's expectation maximization
+""" Choose letter that maximizes mean distance
 
 This approach does not require *any* label/proportion information. Required information:
 - Which symbols can be spelled?
 - For each epoch: which symbols have been highlighted?
 
 Assumptions:
-- Similar to LDA: Both classes have the same covariance
-- Underlying are only 2 different normal distributions, i.e. Target/NonTarget
-- Each letter highlighted the same number of times
-
-Notable non-assumptions:
-- Linear separation (? not really or? Likelihood threshold is linear?...)
+- The true target/nontarget assignments maximize distance between class means
 """
-
-
-def get_initialized_gmm(feature_dim=None):
-    gmm_model = GaussianMixture(
-        weights_init=[52 / 68, 16 / 68],
-        n_components=2,
-        covariance_type="tied",
-        means_init=np.zeros((2, feature_dim)),
-    )
-    return gmm_model
-
-
-def get_true_letter(e):
-    evs = list(e["Target"].event_id.keys())
-    t_letters = [ev.split("/")[3:] for ev in evs]
-    t_let = t_letters[0]
-    for tl in t_letters[1:]:
-        for l in t_let:
-            if l not in tl:
-                t_let.remove(l)
-        if len(t_let) == 1:
-            break
-    return t_let[0]
 
 
 def softmax(x):
@@ -99,7 +68,7 @@ blocks = [1, 2, 3]
 np.random.seed(123)
 
 # Parameters to change
-dcode = "LLP"
+dcode = "Mix"
 plot_erp_after_every_letter = False
 debug_prints = True
 # Idea: instead of using Sigma_T use (Sigma_T - (Mu^T * Mu)) = (Sigma_T - Sigma_B) = Sigma_W ?
@@ -108,7 +77,7 @@ debug_prints = True
 cov_clmean_correction = True
 # Sanity checks
 sanity_check_use_random_mean_for_first_n_letters = 0
-sanity_check_letter_for_data_based_random_means = "G"  # only relevant if previous > 0
+sanity_check_letter_for_data_based_random_means = "G"  # only relevant if previous variable > 0
 sanity_check_random_letter_order = False
 
 if sanity_check_use_random_mean_for_first_n_letters > 0:
@@ -132,9 +101,9 @@ else:
     raise ValueError(f"Dataset code: {dcode} is not valid")
 
 # DEBUG OVERWRITE
-n_letters = 14
-subjects = [11]
-blocks = [3]
+# n_letters = 14
+# subjects = [4, 11]
+# blocks = [3, 1]
 
 parameter_combinations = [[True, True], [False, True], [True, False], [False, False]]
 
@@ -205,26 +174,20 @@ for hyp_i, (use_toeplitz_covariance, use_aggregated_mean) in enumerate(parameter
                 # Store total covariance/scatter. Needed for implementation of only within cov
                 # But that has rank issues for now.
                 total_cov = cov_model.covariance_
+                inversion_time = time.time()
                 total_prec = np.linalg.pinv(total_cov)
-
-                # GMM is only used for likelihood helper functions
-                # Actual EM is NOT run (fit call)
-                gmm = get_initialized_gmm(feature_dim=n_times * n_channels)
-                gmm.covariances_ = total_cov
-                cholesky_time = time.time()
-                gmm.precisions_cholesky_ = _compute_precision_cholesky(gmm.covariances_, "tied")
-                cholesky_duration = time.time() - cholesky_time
+                inversion_duration = time.time() - inversion_time
                 if debug_prints:
-                    print(f"  Cholesky decomposition took {cholesky_duration:1.3f} seconds")
+                    print(f"  Covariance inversion took {inversion_duration:1.3f} seconds")
 
                 # Set up variables
                 # Reset epoch indexing to 0..67
                 epo_current_trial.reset_drop_log_selection()
-                spellable_likelihoods = np.empty(len(spellable)) + np.nan
                 spellable_num_targets = np.empty(len(spellable))
-                spellable_curr_sqdist = np.empty(len(spellable))
-                spellable_agg_sqdist = np.empty(len(spellable))
-                prev_logprob_score = -np.inf
+                # spellable_curr_sqdist = np.empty(len(spellable))
+                # spellable_agg_sqdist = np.empty(len(spellable))
+                spellable_scores = np.empty(len(spellable))
+                prev_best_score = -np.inf
                 best_mean_estimate = None
                 current_trial_X = evec.transform(epo_current_trial)
 
@@ -274,60 +237,45 @@ for hyp_i, (use_toeplitz_covariance, use_aggregated_mean) in enumerate(parameter
                         let_i + 1
                     )
                     if use_aggregated_mean:
-                        gmm.means_ = aggregated_trial_means
+                        agg_mean_diff = (
+                            aggregated_trial_means[1, :] - aggregated_trial_means[0, :]
+                        )[:, None]
+                        # More weight to dimension with less variance
+                        sq_agg_mean_dist = (agg_mean_diff.T @ total_prec @ agg_mean_diff)[0, 0]
+                        # Equal weight to all dimensions
+                        # sq_agg_mean_dist = (agg_mean_diff.T @ agg_mean_diff).squeeze()
+                        score = sq_agg_mean_dist
                     else:
-                        gmm.means_ = current_trial_means
-                    # Potentially apply EM?
-                    # gmm.fit(current_trial_X)
-
-                    # For each datapoint, obtain likelihood of belonging to class 0 or 1
-                    curr_mean_diff = (current_trial_means[1, :] - current_trial_means[0, :])[
-                        :, None
-                    ]
-                    agg_mean_diff = (aggregated_trial_means[1, :] - aggregated_trial_means[0, :])[
-                        :, None
-                    ]
-                    sq_curr_mean_dist = (curr_mean_diff.T @ total_prec @ curr_mean_diff)[0, 0]
-                    sq_agg_mean_dist = (agg_mean_diff.T @ total_prec @ agg_mean_diff)[0, 0]
-
-                    logprob_time = time.time()
-                    logprob = gmm._estimate_log_prob(current_trial_X)
-                    logprob_duration = time.time() - logprob_time
-                    # if debug_prints:
-                    #     print(f" Logprob calculation took {logprob_duration:1.3f} seconds")
-                    logprob_diff = logprob[:, 1] - logprob[:, 0]
-                    # Targets were flashed 16 times: get 16 highest likelihood diffs
-                    # TODO: sometimes epochs are not completely loaded due to marker issues. Still take top16?
-                    top_16 = np.sort(logprob_diff)[-16:]
-                    logprob_score = np.sum(top_16)
+                        curr_mean_diff = (current_trial_means[1, :] - current_trial_means[0, :])[
+                            :, None
+                        ]
+                        sq_curr_mean_dist = (
+                            curr_mean_diff.T @ total_prec @ curr_mean_diff
+                        ).squeeze()
+                        score = sq_curr_mean_dist
 
                     # We want to update the aggregated means with the best estimate for the current
                     # Letter/Trial
-                    if logprob_score > prev_logprob_score:
-                        prev_logprob_score = logprob_score
+                    if score > prev_best_score:
+                        prev_best_score = score
                         best_mean_estimate = current_trial_means
-                    spellable_likelihoods[si] = logprob_score
-                    spellable_agg_sqdist[si] = sq_agg_mean_dist
-                    spellable_curr_sqdist[si] = sq_curr_mean_dist
-                    # print(f"{s}: {logprob_max}")
+                    spellable_scores[si] = score
                 # Update the current class mean aggregate with best trial estimate
                 aggregated_clmeans = (aggregated_clmeans * let_i + best_mean_estimate) / (let_i + 1)
-                # Most likely spellable produces the highest likelihood
-                most_likely_idx = np.argmax(spellable_likelihoods)
+                # Most likely spellable produces highest class mean distances
+                most_likely_idx = np.argmax(spellable_scores)
                 decoded_sentence += spellable[most_likely_idx]
                 letter_evaluation_total_time = time.time() - letter_evaluation_start_time
                 correct = spellable[most_likely_idx] == true_sentence[let_i]
                 # Softmax as approximation of how sure we are, FOR NOW only used as metric.
                 # But: this could be used to inform mean_aggregation, e.g.,
                 # how to weight each trial mean estimate...
-                softmax_best_2 = np.sort(softmax(spellable_likelihoods))[-2:]
-                softmax_best_5 = np.sort(softmax(spellable_likelihoods))[::-1][0:5]
+                softmax_best_2 = np.sort(softmax(spellable_scores))[-2:]
+                softmax_best_5 = np.sort(softmax(spellable_scores))[::-1][0:5]
                 softmax_best_5_str = " ".join(map("{:.4f}".format, softmax_best_5))
-                spellable_order = [spellable[i] for i in np.argsort(spellable_likelihoods)][::-1]
+                spellable_order = [spellable[i] for i in np.argsort(spellable_scores)][::-1]
                 distance_to_true_letter = spellable_order.index(true_sentence[let_i])
                 print(f" Decoded/Actual:  {spellable[most_likely_idx]}/{true_sentence[let_i]}")
-                print(f" According to curr_dist:  {spellable[np.argmax(spellable_curr_sqdist)]}")
-                print(f" According to agg_dist:  {spellable[np.argmax(spellable_agg_sqdist)]}")
                 print(f"  Softmax top5:                {softmax_best_5_str}")
                 print(f"  Best 5 letters (descending): {spellable_order[:5]}\n")
                 softmax_logratio = np.log10(softmax_best_2[1]) - np.log10(softmax_best_2[0])
