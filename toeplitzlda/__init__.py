8182c78739f4fe7bed7f01cc51a9339a2e5bc767
Jan Sosulski <mail@jan-sosulski.de>
2024-02-05 21:58:42 +0100

    Remove much things.

--- toeplitzlda/__init__.py
+++ toeplitzlda/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.2.6"
+__version__ = "0.4.0"
 
-from . import benchmark, classification, usup_replay
+from . import classification
--- toeplitzlda/usup_replay/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import llp
-
-try:
-    from . import visual_speller
-except ImportError:
-    pass
